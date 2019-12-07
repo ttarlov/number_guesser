8f13c8a6d8ca12c13b20b276882e653c146a5b8d
John Watterson <john@breakthrewbrew.com>
2019-12-07 16:04:50 -0700

    Adds clear button functionality

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ ch1InputName.addEventListener('input', validateUserInput);
 ch1InputGuess.addEventListener('input', validateUserInput);
 ch2InputName.addEventListener('input', validateUserInput);
 ch2InputGuess.addEventListener('input', validateUserInput);
+clearBtn.addEventListener('click', clearForm);
 
 window.onload = disableButtons();
 // debugger;
@@ -18,33 +19,42 @@ function disableButtons() {
   submitBtn.disabled = true;
   resetBtn.disabled = true;
   clearBtn.disabled = true;
+  submitBtn.classList.add('button-disabled');
+  resetBtn.classList.add('button-disabled');
+  clearBtn.classList.add('button-disabled');
+
 }
 
 function validateUserInput() {
-  // debugger;
-  if (ch1InputName.value.length === 0 || ch1InputGuess.value.length < 0 ||
-  ch2InputName.value.length === 0 || ch2InputGuess.value.length < 0) {
-    submitBtn.disabled = true;
-    resetBtn.disabled = true;
-    clearBtn.disabled = true;
-  } else if (ch1InputName.value.length !== 0 && ch1InputGuess.value.length !== 0 &&
-   ch2InputName.value.length !== 0 && ch2InputGuess.value.length !== 0) {
+  console.log('Sanity check', ch1InputName.value, ch1InputGuess.value, ch2InputName.value, ch2InputGuess.value);
+  if (ch1InputName.value.length > 0 && ch1InputGuess.value.length > 0 &&
+   ch2InputName.value.length > 0 && ch2InputGuess.value.length > 0) {
+    console.log('second condional');
     submitBtn.disabled = false;
     resetBtn.disabled = false;
     clearBtn.disabled = false;
     submitBtn.classList.remove('button-disabled');
     resetBtn.classList.remove('button-disabled');
     clearBtn.classList.remove('button-disabled');
-  }  else {
+  } else if (ch1InputName.value.length > 0 || ch1InputGuess.value.length > 0 ||
+  ch2InputName.value.length > 0 || ch2InputGuess.value.length > 0) {
+    console.log('first conditional');
     submitBtn.disabled = true;
     resetBtn.disabled = true;
-    clearBtn.disabled = true;
-    submitBtn.classList.add('button-disabled');
-    resetBtn.classList.add('button-disabled');
-    clearBtn.classList.add('button-disabled');
+    clearBtn.disabled = false;
+    clearBtn.classList.remove('button-disabled');
+  } else {
+    console.log('else statement');
+    disableButtons();
   }
 }
 
-
-
-// submitBtn.addEventListener('click', )
+function clearForm() {
+  // debugger;
+  console.log('another sanity check', disableButtons());
+  ch1InputName.value = '';
+  ch1InputGuess.value = '';
+  ch2InputName.value = '';
+  ch2InputGuess.value = '';
+  disableButtons();
+}
